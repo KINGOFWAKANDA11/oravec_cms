81d8b76f54fcc3b078bf0b47538ef7a5c5b62a5a
dejvidEm <bithug10@gmail.com>
2025-05-05 19:30:27 +0200

    fix contact

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -67,7 +67,7 @@ export function Navbar() {
                 isScrolled ? "text-foreground" : "text-white"
               )}
             >
-              {t("nav.contact")}
+              Kontakt
             </Link>
           </nav>
 
